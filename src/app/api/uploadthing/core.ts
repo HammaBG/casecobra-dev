f196121119c97b880254aaff1ab1705538c2a723
HammaBG <mohamedamine00879@gmail.com>
2024-08-23 11:41:05 +0100

    creating configurator page (1)

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,24 +1,52 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import {z} from 'zod';
+import { z } from 'zod';
+import sharp from 'sharp'
+import { db } from '@/db'
 
 const f = createUploadthing();
-  
+
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-  .input(z.object({configId: z.string().optional()}))
+    .input(z.object({ configId: z.string().optional() }))
     // Set permissions and file types for this FileRoute
     .middleware(async ({ input }) => {
-      
+
       return { input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-  
-      const {configId} = metadata.input
 
-      return { configId };
+      const { configId } = metadata.input
+
+      const res = await fetch(file.url)
+      const buffer = await res.arrayBuffer()
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        })
+        return { configId: configuration.id }
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        })
+        
+        return { configId: updatedConfiguration.id }
+      }
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,6 +1,17 @@
-const Page = async () => {
-    // make db call 
+import { db } from '@/db'
+import { notFound } from 'next/navigation'
+//import DesignConfigurator from './DesignConfigurator'
 
-    return <p></p>
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
 }
-export default Page 
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams
+
+return <p>{id}</p>
+}
+
+export default Page
