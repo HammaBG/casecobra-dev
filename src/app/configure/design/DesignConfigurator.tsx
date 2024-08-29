537a6c7606b5473b1c713e22fe2f7e05ad707578
HammaBG <mohamedamine00879@gmail.com>
2024-08-29 12:18:18 +0100

    complete configurator page

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -7,13 +7,18 @@ import { cn, formatPrice } from "@/lib/utils";
 import NextImage from 'next/image';
 import { Rnd } from 'react-rnd';
 import { RadioGroup } from '@headlessui/react'
-import { useRef, useState } from 'react'
+import { use, useRef, useState } from 'react'
 import { COLORS, MODELS, MATERIALS, FINISHES } from "@/validators/option-validator";
 import { Label } from "@/components/ui/label";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Check, ChevronsUpDown } from "lucide-react";
 import { BASE_PRICE } from "@/config/products";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "@/components/ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
+import { saveConfig as _saveConfig, SaveConfigArgs } from "./actions";
+import { useRouter } from "next/navigation";
 
 
 interface DesignConfiguratorProps {
@@ -27,6 +32,25 @@ const DesignConfigurator = ({
     imageUrl,
     imageDimensions,
 }: DesignConfiguratorProps) => {
+    const { toast } = useToast()
+    const router = useRouter()
+
+    const { mutate: saveConfig, isPending } = useMutation({
+        mutationKey: ["save-config"],
+        mutationFn: async (args: SaveConfigArgs) => {
+            await Promise.all([saveConfiguration(), _saveConfig(args)])
+        },
+        onError: () => {
+            toast({
+                title: 'Something went wrong',
+                description: 'There was an error on our end. Please try again.',
+                variant: 'destructive',
+            })
+        },
+        onSuccess: () => {
+            router.push(`/configure/preview?id=${configId}`)
+        },
+    })
     const [options, setOptions] = useState<{
         color: (typeof COLORS)[number]
         model: (typeof MODELS.options)[number]
@@ -39,12 +63,93 @@ const DesignConfigurator = ({
         finish: FINISHES.options[0],
     })
 
+    const [renderedDimension, setRenderedDimension] = useState({
+        width: imageDimensions.width / 4,
+        height: imageDimensions.height / 4,
+    })
+    const [renderedPosition, setRenderedPosition] = useState({
+        x: 150,
+        y: 205,
+    })
+
+    const phoneCaseRef = useRef<HTMLDivElement>(null)
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    const { startUpload } = useUploadThing('imageUploader')
+
+
+    async function saveConfiguration() {
+        try {
+            const {
+                left: caseLeft,
+                top: caseTop,
+                width,
+                height,
+            } = phoneCaseRef.current!.getBoundingClientRect()
+
+            const { left: containerLeft, top: containerTop } =
+                containerRef.current!.getBoundingClientRect()
+
+            const leftOffset = caseLeft - containerLeft
+            const topOffset = caseTop - containerTop
+
+            const actualX = renderedPosition.x - leftOffset
+            const actualY = renderedPosition.y - topOffset
+
+            const canvas = document.createElement('canvas')
+            canvas.width = width
+            canvas.height = height
+            const ctx = canvas.getContext('2d')
+
+            const userImage = new Image()
+            userImage.crossOrigin = 'anonymous'
+            userImage.src = imageUrl
+            await new Promise((resolve) => (userImage.onload = resolve))
+
+            ctx?.drawImage(
+                userImage,
+                actualX,
+                actualY,
+                renderedDimension.width,
+                renderedDimension.height
+            )
+
+            const base64 = canvas.toDataURL()
+            const base64Data = base64.split(',')[1]
+
+            const blob = base64ToBlob(base64Data, 'image/png')
+            const file = new File([blob], 'filename.png', { type: 'image/png' })
+
+            await startUpload([file], { configId })
+
+
+        } catch (err) {
+            toast({
+                title: 'Something went wrong',
+                description:
+                    'There was a problem saving your config, please try again.',
+                variant: 'destructive',
+            })
+
+        }
+    }
+
+    function base64ToBlob(base64: string, mimeType: string) {
+        const byteCharacters = atob(base64)
+        const byteNumbers = new Array(byteCharacters.length)
+        for (let i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i)
+        }
+        const byteArray = new Uint8Array(byteNumbers)
+        return new Blob([byteArray], { type: mimeType })
+    }
+
     return (
         <div className='relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20'>
-            <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
+            <div ref={containerRef} className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
                 <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
 
-                    <AspectRatio ratio={896 / 1831} className='pointer-events-none relative z-50 aspect-[896/1831] w-full'>
+                    <AspectRatio ref={phoneCaseRef} ratio={896 / 1831} className='pointer-events-none relative z-50 aspect-[896/1831] w-full'>
                         <NextImage fill alt='your image' className='pointer-events-none' src="/phone-template.png" />
                     </AspectRatio>
 
@@ -55,6 +160,17 @@ const DesignConfigurator = ({
                     />
                 </div>
                 <Rnd default={{ x: 150, y: 205, height: imageDimensions.height / 4, width: imageDimensions.width / 4 }}
+                    onResizeStop={(_, __, ref, ___, { x, y }) => {
+                        setRenderedDimension({
+                            height: parseInt(ref.style.height.slice(0, -2)),
+                            width: parseInt(ref.style.width.slice(0, -2)),
+                        })
+                        setRenderedPosition({ x, y })
+                    }}
+                    onDragStop={(_, data) => {
+                        const { x, y } = data
+                        setRenderedPosition({ x, y })
+                    }}
                     lockAspectRatio
                     className="absolute z-20 border-[3px] border-primary"
                     resizeHandleComponent={{
@@ -128,7 +244,7 @@ const DesignConfigurator = ({
                                         </DropdownMenuContent>
                                     </DropdownMenu>
                                 </div>
-                                {[MATERIALS,FINISHES].map(({name , options:selectableOptions})=> (
+                                {[MATERIALS, FINISHES].map(({ name, options: selectableOptions }) => (
                                     <RadioGroup key={name} value={options[name]} onChange={(val) => {
                                         setOptions((prev) => ({
                                             ...prev,
@@ -136,20 +252,20 @@ const DesignConfigurator = ({
                                         }))
                                     }}>
                                         <Label>
-                                            {name.slice(0,1).toUpperCase()+name.slice(1)}
+                                            {name.slice(0, 1).toUpperCase() + name.slice(1)}
                                         </Label>
                                         <div className="mt-3 space-y-4">
-                                            {selectableOptions.map((option)=>(
+                                            {selectableOptions.map((option) => (
                                                 <RadioGroup.Option key={option.value} value={option}
-                                                className={({active,checked})=>cn("relative block bg-white cursor-pointer rounded-lg px-4 py-6 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between",{
-                                                    "border-primary" :active || checked
-                                                })}>
+                                                    className={({ active, checked }) => cn("relative block bg-white cursor-pointer rounded-lg px-4 py-6 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between", {
+                                                        "border-primary": active || checked
+                                                    })}>
                                                     <span className="flex items-center">
                                                         <span className="flex flex-col text-sm">
                                                             <RadioGroup.Label as="span" className="font-medium text-gray-900">
                                                                 {option.label}
                                                             </RadioGroup.Label>
-                                                            {option.description ?(
+                                                            {option.description ? (
                                                                 <RadioGroup.Description as="span" className="text-gray-500">
                                                                     <span className="block sm:inline">{option.description}</span>
                                                                 </RadioGroup.Description>
@@ -158,7 +274,7 @@ const DesignConfigurator = ({
                                                     </span>
                                                     <RadioGroup.Description as="span" className="mt-2 flex text-sm sm:ml-4 sm:mt-0 sm-flex-col sm:text-right">
                                                         <span className="font-medium text-gray-900">
-                                                            {formatPrice(option.price/100)}
+                                                            {formatPrice(option.price / 100)}
                                                         </span>
                                                     </RadioGroup.Description>
                                                 </RadioGroup.Option>
@@ -175,11 +291,17 @@ const DesignConfigurator = ({
                     <div className="w-full h-full flex justify-end items-center">
                         <div className="w-full flex gap-6 items-center">
                             <p className="font-medium whitespace-nowrap">
-                                {formatPrice((BASE_PRICE + options.finish.price +options.material.price)/100)}
+                                {formatPrice((BASE_PRICE + options.finish.price + options.material.price) / 100)}
                             </p>
-                            <Button size="sm" className="w-full">
+                            <Button onClick={() => saveConfig({
+                                configId,
+                                color: options.color.value,
+                                finish:options.finish.value,
+                                material:options.material.value,
+                                model:options.model.value,
+                            })} size="sm" className="w-full">
                                 Continue
-                                <ArrowRight  className="h-4 w-4 ml-1.5 inline"/>
+                                <ArrowRight className="h-4 w-4 ml-1.5 inline" />
                             </Button>
                         </div>
                     </div>
