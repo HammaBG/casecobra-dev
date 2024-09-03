6031654c35af7a4e1666bb5a1447588f5ab5debf
HammaBG <mohamedamine00879@gmail.com>
2024-09-03 13:26:42 +0100

    adding stripe

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -2,7 +2,9 @@
 
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products"
 import { db } from "@/db"
+import { stripe } from "@/lib/stripe"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { Order } from "@prisma/client"
 
 
 export const createCheckoutSession = async ({
@@ -29,4 +31,44 @@ export const createCheckoutSession = async ({
  if (finish === "textured") price += PRODUCT_PRICES.finish.textured
  if (material === "polycarbonate") price += PRODUCT_PRICES.material.polycarbonate
 
+ let order  : Order | undefined = undefined
+
+ const existingOrder = await db.order.findFirst({
+    where :  {
+        userId : user.id,
+        configurationId: configuration.id,
+    },
+ })
+
+ if (existingOrder) {
+    order = existingOrder
+ } else {
+    order = await db.order.create ({
+        data : {
+            amount: price /100, 
+            userId: user.id,
+            configurationId: configuration.id,
+        }
+    })
+ }
+
+ const product = await stripe.products.create({
+    name : 'Custom iPhone Case',
+    images: [configuration.imageUrl],
+    default_price_data: {
+        currency: "USD",
+        unit_amount:price,
+    }
+ })
+ const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ['card' , 'paypal'],
+    mode: "payment",
+    shipping_address_collection: {allowed_countries: ['DE' , 'US']},
+    metadata: {
+        userId : user.id,
+        orderId: order.id,
+    }
+ })
 }
